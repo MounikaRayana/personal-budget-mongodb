f0eafe1262abfda9f48dc03f6860f2cc011375b7
Mounika Rayana <rayana.mounika@gmail.com>
2020-10-04 01:45:40 -0400

    Loading data from JSON file

--- server.js
+++ server.js
@@ -3,24 +3,9 @@ const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
+'use strict';
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 25
-    },
-    {
-        title: 'Rent',
-        budget: 275   
-    },
-    {
-        title: 'Grocery',
-        budget: 110
-    },
-
-]
-};
+const budget = require('./budget.json');
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
